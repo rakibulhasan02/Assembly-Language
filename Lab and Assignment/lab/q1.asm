b25357cd57a01a317a0f7af6d861c67170790c5a
Md Rakibul Hasan <152282042+rakibulhasan02@users.noreply.github.com>
2025-08-08 15:18:53 +0600

    Update q1.asm

--- Lab and Assignment/lab/q1.asm
+++ Lab and Assignment/lab/q1.asm
@@ -2,55 +2,53 @@
 .model small
 .stack 100h
 .data
-m db "Enter a upper case letter :$"
-nl db 0dh,0ah,"$"
-m1 db "Lower case: $"
-m2 db "Upper case: $"
-
+m db 0dh,0ah,"Enter a uppercase letter: $"
+m1 db 0dh,0ah,"Enter a lowercase letter: $"
+m2 db 0dh,0ah, "Lower case: $"
+m3 db 0dh,0ah,"Upper case: $"
 .code
 main proc
     mov ax,@data
     mov ds,ax
     
-    ;print prompt
     mov ah,9
     lea dx,m
     int 21h
-    ;read character
     mov ah,1
-    int 21h  
+    int 21h 
+    add al,32
+    mov bl,al  
     
-    add al,32    ;upper case to lower case
-    mov bl,al    
-    sub al,32     ;lower to upper case
-    mov  cl,al
+    ;call newline   
     
-    ;newline
-    mov ah,9 
-    lea dx,nl
-    int 21h
-    ;print lower case
+    mov ah,9
     lea dx,m1
     int 21h
-   
-    mov ah,2
-    mov dl,bl
+    mov ah,1
     int 21h 
-    ;newline
-    mov ah,9 
-    lea dx,nl
-    int 21h 
+    sub al,32
+    mov cl,al
     
-      ;print upper case
-    lea dx,m2
-    int 21h
-   
-    mov ah,2
-    mov dl,cl
-    int 21h  
+     ;call newline 
+     
+     mov ah,9
+     lea dx,m2
+     int 21h
+     mov ah,2
+     mov dl,bl
+     int 21h
+     
+     ;call newline 
+     mov ah,9
+     lea dx,m3
+     int 21h
+     mov ah,2
+     mov dl,cl
+     int 21h
     
     ;exit
     mov ah,4ch
     int 21h
 main endp
+
 end main
